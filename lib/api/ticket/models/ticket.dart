9bb4cf6a75c01d10f67bd913098e55314fa9d94a
Michael Safwat Hanna <michaelsafwat.hanna@gmail.com>
2021-04-27 18:42:12 +0200

    refactor: add user key to ticket (#11)
    Refs tazkrtak/server#16, tazkrtak/ccimulator#3

--- lib/api/ticket/models/ticket.dart
+++ lib/api/ticket/models/ticket.dart
@@ -1,18 +1,24 @@
 import 'dart:convert';
 
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'ticket.g.dart';
+
+@JsonSerializable(createFactory: false)
 class Ticket extends Equatable {
   final String userId;
   final String totp;
+  final String userKey;
   final int quantity;
   final double price;
 
-  String get value => '$userId:$totp:$quantity:$price';
+  String get value => _toJsonString();
   String get encodedValue => base64Encode(utf8.encode(value));
 
   const Ticket({
     required this.userId,
+    required this.userKey,
     required this.totp,
     required this.quantity,
     required this.price,
@@ -20,17 +26,25 @@ class Ticket extends Equatable {
 
   Ticket copyWith({
     String? userId,
+    String? userKey,
     String? totp,
     int? quantity,
     double? price,
   }) =>
       Ticket(
         userId: userId ?? this.userId,
+        userKey: userKey ?? this.userKey,
         totp: totp ?? this.totp,
         quantity: quantity ?? this.quantity,
         price: price ?? this.price,
       );
 
   @override
-  List<Object> get props => [userId, totp, quantity, price];
+  List<Object> get props => [userId, userKey, totp, quantity, price];
+
+  Map<String, dynamic> toJson() => _$TicketToJson(this);
+
+  String _toJsonString() {
+    return const JsonEncoder.withIndent(null).convert(toJson());
+  }
 }
