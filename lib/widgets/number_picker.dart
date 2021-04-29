a9f5f0e9bb1c329f4d12c8c7806ef88c588f95e7
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2021-04-29 15:01:58 +0200

    fix: remove picker margin

--- lib/widgets/number_picker.dart
+++ lib/widgets/number_picker.dart
@@ -23,74 +23,71 @@ class NumberPicker<T extends num> extends HookWidget {
     final state = useState(initialValue ?? minimum);
     final defaultStep = step ?? 1;
 
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 32),
-      child: Stack(
-        alignment: AlignmentDirectional.center,
-        fit: StackFit.passthrough,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              height: 64,
-              decoration: BoxDecoration(
-                color: Theme.of(context).highlightColor,
-                // color: Colors.black26,
-              ),
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      fit: StackFit.passthrough,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            height: 64,
+            decoration: BoxDecoration(
+              color: Theme.of(context).highlightColor,
+              // color: Colors.black26,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              children: [
-                Text(
-                  '${state.value}',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Text(
-                  label,
-                  style: const TextStyle(
-                      color: Colors.black87,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24),
-                ),
-                const Spacer(),
-                GestureDetector(
-                  onLongPress: () {
-                    state.value = minimum;
-                    onChange(minimum);
-                  },
-                  child: CircularIconButton(
-                    color: Theme.of(context).errorColor,
-                    icon: LineAwesomeIcons.minus,
-                    onPressed: () {
-                      if (state.value <= minimum) {
-                        return;
-                      }
-                      state.value = state.value - defaultStep as T;
-                      onChange(state.value);
-                    },
-                  ),
-                ),
-                const SizedBox(width: 8),
-                CircularIconButton(
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Text(
+                '${state.value}',
+                style: TextStyle(
                   color: Theme.of(context).primaryColor,
-                  icon: LineAwesomeIcons.plus,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(width: 16),
+              Text(
+                label,
+                style: const TextStyle(
+                    color: Colors.black87,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24),
+              ),
+              const Spacer(),
+              GestureDetector(
+                onLongPress: () {
+                  state.value = minimum;
+                  onChange(minimum);
+                },
+                child: CircularIconButton(
+                  color: Theme.of(context).errorColor,
+                  icon: LineAwesomeIcons.minus,
                   onPressed: () {
-                    state.value = state.value + defaultStep as T;
+                    if (state.value <= minimum) {
+                      return;
+                    }
+                    state.value = state.value - defaultStep as T;
                     onChange(state.value);
                   },
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(width: 8),
+              CircularIconButton(
+                color: Theme.of(context).primaryColor,
+                icon: LineAwesomeIcons.plus,
+                onPressed: () {
+                  state.value = state.value + defaultStep as T;
+                  onChange(state.value);
+                },
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
