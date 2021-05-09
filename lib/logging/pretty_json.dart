a66af12704eaf473eca4c48beee0bcb8949ddf17
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-05-09 03:50:29 +0200

    refactor: log response's body with error

--- lib/logging/pretty_json.dart
+++ lib/logging/pretty_json.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
-String prettyJson(dynamic source, {int indent = 2}) {
+String prettyJson(dynamic? source, {int indent = 2}) {
+  if (source == null) return '{}';
   final spaces = ' ' * indent;
   final encoder = JsonEncoder.withIndent(spaces);
   return encoder.convert(source);
