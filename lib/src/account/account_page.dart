c227e1bdd229119da98afb0424fd61c420432e02
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-05-23 23:59:09 +0200

    patch-2

--- lib/src/account/account_page.dart
+++ lib/src/account/account_page.dart
@@ -10,71 +10,73 @@ import '../register/models/models.dart';
 class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final sessionCubit = context.read<SessionCubit>();
-    final user = (sessionCubit.state as SessionSuccess).user;
-    final phoneMaskFormatter =
-        MaskTextInputFormatter(mask: PhoneNumber.kMaskFormat);
-    final nationalIdMaskFormatter =
-        MaskTextInputFormatter(mask: NationalId.kMaskFormat);
+    final phoneFormatter = MaskTextInputFormatter(
+      mask: PhoneNumber.kMaskFormat,
+    );
+    final nationalIdFormatter = MaskTextInputFormatter(
+      mask: NationalId.kMaskFormat,
+    );
+
     return Scaffold(
-      body: BlocBuilder<SessionCubit, SessionState>(
-        builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.only(top: 40, right: 16),
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(
-                    LineAwesomeIcons.user,
-                    size: 30,
-                    color: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: BlocBuilder<SessionCubit, SessionState>(
+            // Prevents rebuilding the page when the user
+            // signs out (i.e. state changes to SessionUnknown)
+            buildWhen: (_, current) => current is SessionSuccess,
+            builder: (context, state) {
+              final user = (state as SessionSuccess).user;
+              return Column(
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      LineAwesomeIcons.user,
+                      size: 30,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text(tr(context).register_fullNameHint),
+                    subtitle: Text(user.fullName),
                   ),
-                  title: Text(tr(context).register_fullNameHint),
-                  subtitle: Text(user.fullName),
-                ),
-                ListTile(
-                  leading: Icon(
-                    LineAwesomeIcons.phone,
-                    size: 30,
-                    color: Theme.of(context).primaryColor,
+                  ListTile(
+                    leading: Icon(
+                      LineAwesomeIcons.phone,
+                      size: 30,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text(tr(context).register_phoneNumberHint),
+                    subtitle: Text(phoneFormatter.maskText(user.phoneNumber)),
                   ),
-                  title: Text(
-                    tr(context).register_phoneNumberHint,
+                  ListTile(
+                    leading: Icon(
+                      LineAwesomeIcons.at,
+                      size: 30,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text(tr(context).register_emailHint),
+                    subtitle: Text(user.email),
                   ),
-                  subtitle: Text(phoneMaskFormatter.maskText(user.phoneNumber)),
-                ),
-                ListTile(
-                  leading: Icon(
-                    LineAwesomeIcons.at,
-                    size: 30,
-                    color: Theme.of(context).primaryColor,
+                  ListTile(
+                    leading: Icon(
+                      LineAwesomeIcons.identification_card_1,
+                      size: 30,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text(tr(context).register_nationalIdHint),
+                    subtitle:
+                        Text(nationalIdFormatter.maskText(user.nationalId)),
                   ),
-                  title: Text(tr(context).register_emailHint),
-                  subtitle: Text(user.email),
-                ),
-                ListTile(
-                  leading: Icon(
-                    LineAwesomeIcons.identification_card_1,
-                    size: 30,
-                    color: Theme.of(context).primaryColor,
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: () =>
+                        context.read<SessionCubit>().clearSession(),
+                    child: Text(tr(context).account_sign_out),
                   ),
-                  title: Text(tr(context).register_nationalIdHint),
-                  subtitle:
-                      Text(nationalIdMaskFormatter.maskText(user.nationalId)),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                ElevatedButton(
-                  onPressed: () => context.read<SessionCubit>().clearSession(),
-                  child: Text(
-                    tr(context).account_sign_out,
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
+                ],
+              );
+            },
+          ),
+        ),
       ),
     );
   }
