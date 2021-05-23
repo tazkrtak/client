66248bd9838df35b9ab3ee4d02635bcc3ce89727
monicatanios <monicatanios@gmail.com>
2021-05-23 17:30:42 +0200

    Patch 1

--- lib/src/account/account_page.dart
+++ lib/src/account/account_page.dart
@@ -1,35 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
 import '../../l10n/tr.dart';
 import '../app/cubits/session_cubit.dart';
+import '../register/models/models.dart';
 
 class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final sessionCubit = context.read<SessionCubit>();
     final user = (sessionCubit.state as SessionSuccess).user;
+    final phoneMaskFormatter =
+        MaskTextInputFormatter(mask: PhoneNumber.kMaskFormat);
+    final nationalIdMaskFormatter =
+        MaskTextInputFormatter(mask: NationalId.kMaskFormat);
     return Scaffold(
       body: BlocBuilder<SessionCubit, SessionState>(
         builder: (context, state) {
           return Padding(
             padding: const EdgeInsets.only(top: 40, right: 16),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                ElevatedButton(
-                  onPressed: () => context.read<SessionCubit>().clearSession(),
-                  child: Text(
-                    tr(context).account_sign_out,
-                  ),
-                ),
-                const Center(
-                  child: Icon(
-                    LineAwesomeIcons.user,
-                    size: 180,
-                  ),
-                ),
-                // const SizedBox(height: 32),
                 ListTile(
                   leading: Icon(
                     LineAwesomeIcons.user,
@@ -45,8 +38,10 @@ class AccountPage extends StatelessWidget {
                     size: 30,
                     color: Theme.of(context).primaryColor,
                   ),
-                  title: Text(tr(context).register_phoneNumberHint),
-                  subtitle: Text(user.phoneNumber),
+                  title: Text(
+                    tr(context).register_phoneNumberHint,
+                  ),
+                  subtitle: Text(phoneMaskFormatter.maskText(user.phoneNumber)),
                 ),
                 ListTile(
                   leading: Icon(
@@ -64,7 +59,17 @@ class AccountPage extends StatelessWidget {
                     color: Theme.of(context).primaryColor,
                   ),
                   title: Text(tr(context).register_nationalIdHint),
-                  subtitle: Text(user.nationalId),
+                  subtitle:
+                      Text(nationalIdMaskFormatter.maskText(user.nationalId)),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  onPressed: () => context.read<SessionCubit>().clearSession(),
+                  child: Text(
+                    tr(context).account_sign_out,
+                  ),
                 ),
               ],
             ),
